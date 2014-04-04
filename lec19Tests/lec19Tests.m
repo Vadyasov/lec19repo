0ac233f6eaa65dfa0904dea89cb416d55523a6dc
Konstantin Vadyasov <vadyasov@air-konstantin.lan>
2014-04-04 19:13:35 +0300

    Initial Commit

---
+++ lec19Tests/lec19Tests.m
@@ -0,0 +1,34 @@
+//
+//  lec19Tests.m
+//  lec19Tests
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lec19Tests : XCTestCase
+
+@end
+
+@implementation lec19Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
