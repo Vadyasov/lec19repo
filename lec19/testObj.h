ca071d2f028a4dda90e0c71c5b4ceb3c8251359d
Konstantin Vadyasov <vadyasov@air-konstantin.lan>
2014-04-04 19:40:17 +0300

    new obj

---
+++ lec19/testObj.h
@@ -0,0 +1,13 @@
+//
+//  testObj.h
+//  lec19
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testObj : NSObject
+
+@end
---
+++ lec19/testObj.m
@@ -0,0 +1,13 @@
+//
+//  testObj.m
+//  lec19
+//
+//  Created by Konstantin Vadyasov on 04.04.14.
+//  Copyright (c) 2014 Konstantin Vadyasov. All rights reserved.
+//
+
+#import "testObj.h"
+
+@implementation testObj
+
+@end
